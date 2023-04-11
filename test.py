6852ecb92db6aee1ab28340863fa5a331b87595b
Gokocum <gkbrk_5@hotmail.com>
2023-04-11 10:05:28 +0200

    fourth commit

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-def test_git(a,b):
-    a=int,
-    b=int,
-    print("This is ", a, " test for git")
-    print("This is ", b, "additional print command")
-test_git(1,2)
+def test_git():
+    print("Fourth commit")
+test_git()
