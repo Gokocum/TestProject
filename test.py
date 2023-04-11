1fa669804b0b5c0757e8495aa7c6b0972de03bd4
Gokocum <gkbrk_5@hotmail.com>
2023-04-11 10:02:06 +0200

    third commit

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-def test_git():
-    print("This is a test for git")
-    print("This is an additional print command")
-test_git()
+def test_git(a,b):
+    a=int,
+    b=int,
+    print("This is ", a, " test for git")
+    print("This is ", b, "additional print command")
+test_git(1,2)
